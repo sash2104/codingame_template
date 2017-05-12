2c190f9951bcc6c0d36950edf94d23eda94ce2d6
sash <sash2104@gmail.com>
2017-05-13 07:20:16 +0900

    [ADD] Action and Command class

--- cpp/game.cpp
+++ cpp/game.cpp
@@ -3,6 +3,7 @@
 #include "game.hpp"
 
 Game::Game() {
+  turn = 1;
 }
 
 Game::~Game() {
@@ -15,7 +16,18 @@ void Game::read_initial_input() {
 }
 
 void Game::read_input() {
+  turn += 1;
 }
 
 void Game::write_output() {
+}
+
+void Game::determine_command() {
+  simulate();
+}
+
+void Game::simulate() {
+}
+
+void Game::revert() {
 }
--- cpp/game.hpp
+++ cpp/game.hpp
@@ -3,12 +3,43 @@
 #include <unordered_map>
 #include "pos.hpp"
 
+enum struct Action
+{
+  NONE,
+  // revert commands
+  // SET_DUMMY
+};
+
+struct Command
+{
+  Command(Action action, int id, int value)
+    : action(action), id(id), value(value) {}
+  Command(Action action, int id)
+    : action(action), id(id) {}
+  Command() {}
+  Action action;
+  int id;
+  int value; // Actionに応じてspeed, orientationなど格納される値が変わる
+
+  friend std::ostream& operator<< (std::ostream &out, const Command &c)
+  {
+    // out << "hoge";
+    return out;
+  }
+};
+
 class Game {
   public:
+    int turn;
     Game();
     ~Game();
     void write_initial_output();
     void read_initial_input();
     void read_input();
     void write_output();
+
+    void determine_command();
+    void clear();
+    void simulate(); // simulate a turn
+    void revert(); // revert commands
 };
